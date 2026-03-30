b72d39e838d9c32418bbe2df6a64bcfbaab49b0a
Nguyen Anh Tuan <bitogl00z@gmail.com>
2026-03-31 00:04:20 +0700

    chuẩn bị deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,14 +4,24 @@ import express from "express";
 import taskRoute from "./routes/tasksRouters.js";
 import { connectDB } from "./config/db.js";
 import cors from "cors";
-
+import path from "path";
 const PORT = process.env.PORT || 5001;
 const app = express();
+const __dirname = path.resolve();
 //middlewares
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173" }));
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({ origin: "http://localhost:5173" }));
+}
 app.use("/api/tasks", taskRoute);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Server bắt đầu chạy trên cổng ${PORT}`);
